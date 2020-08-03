2cb243e920b447a8967312bc40cb58424b71ea99
Daniyal Zakir <daniyalzakir03@gmail.com>
2020-08-03 22:52:47 +0500

    Assignment -11 Quiz Web App Responsive

--- app.js
+++ app.js
@@ -1,149 +1,46 @@
+// Name : M.Daniyal Zakir
+// Roll No: WM5912
+// Email: daniyalzakir03@gmail.com
+// Assignment-11: QUIZ WEB APP
+
 questionsArray = [
 {
 
     question: "Q1. Who is the inventor of Facebook?",
-    answer: "Mark Zuckerburg",
-    options: ["Mark Zuckerburg", "Steve Jobs", "Bill Gates", "Elon Musk"]
+    answer: "1. Mark Zuckerburg",
+    options: ["1. Mark Zuckerburg", "2. Steve Jobs", "3. Bill Gates", "4. Elon Musk"]
 
 },
 {
 
     question: "Q2. Microsoft founder is?",
-    answer: "Bill Gates",
-    options: ["Bill Gates", "Mark Zuckerburg", "David Backhem", "Carlos Slim"]
-
+    answer: "1. Bill Gates",
+    options: ["1. Bill Gates", "2. Mark Zuckerburg", "3. David Backhem", "4. Carlos Slim"]
 
 },
 {
 
     question: "Q3. What is the abbrevation of Google?",
-    answer: "Global Organization of Oriented Group Language of Earth",
-    options: ["Not given", "All Of them", "Global Organization Oriented Group Language Earth", "Global Organization of Oriented Group Language of Earth"]
-
+    answer: "4. Global Organization of Oriented Group Language of Earth",
+    options: ["1. Not given", "2. All Of them", "3. Global Organization Oriented Group Language Earth", "4. Global Organization of Oriented Group Language of Earth"]
 
 },
 {
 
     question: "Q4. If a computer has more than one processor then it is known as?",
-    answer: "Multiprocessor",
-    options: ["Uni-process", "Multiprocessor", "Multi-threaded", "Multi programming"]
-
+    answer: "2. Multiprocessor",
+    options: ["1. Uni-process", "2. Multiprocessor", "3. Multi-threaded", "4. Multi programming"]
 
 },
 {
 
-
     question: "Q5. WWW stands for?",
-    answer: "World Wide Web",
-    options: ["World Wide Web", "Whole World Web", "Webd Wide World", "Nonve of them"]
-
+    answer: "1. World Wide Web",
+    options: ["1. World Wide Web", "2. Whole World Web", "3. Web Wide World", "4. None of them"]
 
 } ]
 
-
-function quizStarted(){
-sweetAlertSuccessMsg("Your Quiz Has Been Started")
-}
-
-
-// Timer
-var min = 0
-var sec = 0
-var ms = 0
-var getMin = document.getElementById("min")
-var getSec = document.getElementById("sec")
-var interval
-
-function millisecond() {
-    ms++
-    if (ms >= 100) {
-        sec++
-        getSec.innerHTML = sec
-        ms = 0
-    }
-
-    if (sec >= 59) {
-        min++
-        getMin.innerHTML = min
-        sec = 0
-    }
-
-    // Result Page
-    if(min==10){
-    location.replace("result.html") 
-    }
-}
-function start() {
-    interval = setInterval(millisecond, 10);
-}
-start()
-
-
-
-
-
-// Add Active Class
-function addClassActive(e){
-removeClassActive()
-e.classList.add("active")
-}
-// Remove Active Class
-function removeClassActive(){
-var active= document.getElementsByClassName("active")
-for(var i=0; i<active.length; i++){
-active[i].classList.remove("active") 
-}
-}
-
-
-// LogIn Avatar
-function login(){
-sweetAlertSuccessMsg("You are LogIn")
-}
-
-
-var count=0
-function nextQuestionCount(){
-count++ 
-nextQuestion(count)
-score(count)
-
-}
-
-
-function nextQuestion(e){
-// Submit Quiz
-if([e]==5){
-sweetAlertSuccessMsg("Submitting Quiz")
-location.replace("result.html") 
-}
-
-// Questions
-var Question= document.getElementById("question");
-Question.innerHTML=questionsArray[e].question;
-
-// Answers
-var Options= document.getElementsByClassName("p-2")
-for(var i=0; i<Options.length; i++){
-Options[i].innerHTML=[i+1] +". "+questionsArray[e].options[i]
-}
-
-// Question 5/5
-var questionCount= document.getElementById("questionCount");
-questionCount.innerHTML=e+1
-
-removeClassActive()
-}
-
-
-
-
-
-
-
-
-
-/* Main Html*/
+/*=============== Index Html ============*/
 function inp(){
 var input= document.getElementById("input")
 if(input.value==""){
@@ -158,46 +55,165 @@ localStorage.setItem("ls", input.value);
 // Local Storage Getting Data
 document.getElementById("name").innerHTML ="Hello! "+ localStorage.getItem("ls");
 
-// For Removing Local Storage Data
-// localStorage.removeItem(ls);
+    
+    
 
-
-
-
-/* Result Html*/
-var score= 0
-// var score= document.getElementById("score")
-function score(e){
-    var sco= document.getElementById("score")
-   
-    // var active= document.getElementsByClassName("active")
-    if(active[0].innerHTML==questionsArray[e].answer){
-        sco.innerHTML= "aa"
-        // score +=2
-    // localStorage.setItem("sc", score.value);
-    // break
-
-    // if(score>=4 && score<=10){
-    // sweetAlertSuccessMsg("Congratulations! You Are Pass")
-    // }
-    // else{
-    // sweetAlertSuccessMsg("Sorry! You are Fail")
-    // }
- }
+/*================ Main Html ==============*/
+// Quiz Started Message & Timer Start
+function quizStarted(){
+sweetAlertSuccessMsg("Your Quiz Has Been Started")
+start()
 }
-// document.getElementById("score").innerHTML =localStorage.getItem("sc");
+
+// Timer
+var min=0, sec=0, ms=0, interval=0;
+var getMin = document.getElementById("min")
+var getSec = document.getElementById("sec")
+
+function millisecond() {
+ms++
+if (ms >= 100) {
+sec++
+getSec.innerHTML = sec
+ms = 0
+}
+if (sec >= 59) {
+min++
+getMin.innerHTML = min
+sec = 0
+// Result Page If Time Over
+if(min==10){
+sweetAlertTimer("Your Time Is Over!<br> Submitting Quiz") }
+}
+}
+function start() {
+interval = setInterval(millisecond, 10);
+}
 
 
 
 
+// Add Active Class
+function addClassActive(e){
+removeClassActive()
+e.classList.add("active")
+}
+// Remove Active Class
+function removeClassActive(){
+var active= document.getElementsByClassName("active")
+for(var i=0; i<active.length; i++){
+active[i].classList.remove("active") }
+}
+
+
+
+// LogIn Avatar
+function login(){
+sweetAlertSuccessMsg("You are LogIn")
+}
+// LogOut
 function logOut(){
-    location.replace("index.html") 
+location.replace("index.html")
+// For Removing Local Storage Data
+localStorage.removeItem(ls);
+localStorage.removeItem(result);
 }
 
 
 
- 
-// Sweet Alert Library
+
+
+// Next Question
+var count= 0
+function nextQuestionCount(){
+// Active Class is Selected or Not
+var active= document.getElementsByClassName("active")
+if (!$(active).hasClass("active")){
+Swal.fire("Please Select An Option")
+}
+else{
+count++ 
+// nextQuestion
+nextQuestion(count)
+// Scores
+scores(count)
+// Remove Class Active
+removeClassActive()
+}
+}
+
+function nextQuestion(e){
+// Submit Quiz
+if([e]==5){
+sweetAlertTimer("Submitting Quiz") 
+}
+else{
+// Questions
+var Question= document.getElementById("question")
+Question.innerHTML=questionsArray[e].question;
+// Answers
+var Options= document.getElementsByClassName("p-2")
+for(var i=0; i<Options.length; i++){
+Options[i].innerHTML=questionsArray[e].options[i] 
+}
+// removeClassActive()
+// Question Count 5/5
+var questionCount= document.getElementById("questionCount")
+questionCount.innerHTML=e+1
+}
+}
+
+
+
+
+
+
+
+/*================ Result Html ==============*/
+var sc= 0
+function scores(e){
+  
+var active= document.getElementsByClassName("active")
+if(active[0].innerHTML==questionsArray[e].answer){
+sc+=2
+
+console.log(e)
+console.log(sc)
+console.log( active[0].innerHTML)
+console.log(questionsArray[e].answer)
+localStorage.setItem("result", sc);
+}
+else{
+console.log("**** ")
+}
+console.log("C: "+ sc)
+console.log("A: "+ active[0].innerHTML)
+console.log("B: "+questionsArray[e].answer)
+console.log("**** ")
+}
+
+
+// Result With Local Strorage  
+function result(){
+var score= document.getElementById("score")
+result=localStorage.getItem("result")
+score.innerHTML=result
+
+if(result>=4){
+sweetAlertSuccessMsg("Congratulations! You Are Pass")
+}
+else{
+sweetAlertSuccessMsg("Sorry! You are Fail")
+}
+}
+
+
+
+
+
+
+/*================ Sweet Alert Library ==============*/
+// Sweet Alert Library Message
 function sweetAlertSuccessMsg(msg){
     const Toast = Swal.mixin({
     toast: true,
@@ -214,4 +230,38 @@ function sweetAlertSuccessMsg(msg){
     icon: 'success',
     title: msg
   })  
+}
+
+// Sweet Alert Library Timer
+function sweetAlertTimer(timer){
+let timerInterval
+Swal.fire({
+  title: timer,
+  html: ' Wait a moment... ',
+  timer: 3000,
+  timerProgressBar: true,
+  onBeforeOpen: () => {
+    Swal.showLoading()
+    timerInterval = setInterval(() => {
+      const content = Swal.getContent()
+      if (content) {
+        const b = content.querySelector('b')
+        if (b) {
+            b.textContent = Swal.getTimerLeft()
+        }
+      }
+    }, 100)
+  },
+  onClose: () => {
+    //   Timer Close 
+    location.replace("result.html")
+
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
 }
